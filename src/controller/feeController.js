91a61ff5c4f3a18c339cb47e1a6450ee28cdb729
OCHOWO <ochowo@Ochowo.local>
2022-02-28 12:44:41 +0100

    clean create configuration

--- src/controller/feeController.js
+++ src/controller/feeController.js
@@ -7,10 +7,6 @@ export const createConfiguration = async (req, res) => {
   const { FeeConfigurationSpec } = req.body;
   const arr = await parseRequest(FeeConfigurationSpec);
   const data = JSON.stringify(arr);
-  const dd = await redisClient.set('data', data);
-  if (dd) {
-    await redisClient.get('data');
-    return res.send({ status: [dd, 'oo'] });
-  }
-  return res.send({ status: data });
+  redisClient.set('data', data);
+  return res.send({ status: 'ok' });
 };
